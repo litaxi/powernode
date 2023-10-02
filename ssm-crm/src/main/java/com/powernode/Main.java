eef5a68a82c44fc21b1ef4480d11d2b41d2cab37
litongxiang <litongxiang1997@126.com>
2023-10-02 16:18:52 +0800

    crm空工程

---
+++ ssm-crm/src/main/java/com/powernode/Main.java
@@ -0,0 +1,10 @@
+package com.powernode;
+
+/**
+ * @Author wataxi @Date 2023/10/2 16:17 @Version 1.0
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
